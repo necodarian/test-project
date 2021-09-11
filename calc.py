6816c06a4299c9319ebb6f4bed952a60d6a59cc7
necodarian <neco.d@outlook.com>
2021-09-11 18:55:59 +0200

    reformated the file

--- calc.py
+++ calc.py
@@ -1,21 +1,21 @@
-
-
 def add(a, b):
-    return a+b
+    return a + b
+
 
 def sub(a, b):
-    return a-b
+    return a - b
+
 
 def div(a, b):
     if b == 0:
         return None
     else:
-        return a/b
+        return a / b
+
 
 def mul(a, b):
     return a * b
 
 
 if __name__ == "__main__":
-    print(f"addition of {3} and {5} is equal to {add(3,5)}")
-
+    print(f"addition of {3} and {5} is equal to {add(3, 5)}")
